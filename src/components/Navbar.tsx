b3a998660650ad68674d1d9d326f401469ecf4b6
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-09 21:28:15 +0530

    update render logic for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 import { MdClose } from "react-icons/md";
 import { CiMenuFries } from "react-icons/ci";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const router = useRouter();
+  const pathname = usePathname();
   const handleLogout = () => {
     localStorage.removeItem("student_data");
     router.push("/");
@@ -26,18 +27,33 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="hidden md:flex space-x-4">
-          <Link
-            href="/books"
-            className="text-white 2xl mt-1 mx-3 hover:text-gray-500"
-          >
-            Find Books
-          </Link>
-          <button
-            onClick={handleLogout}
-            className="text-white text-lg hover:text-gray-500"
-          >
-            Logout
-          </button>
+          {pathname !== "/" && pathname !== "/students" && (
+            <Link
+              href="/books"
+              className={`text-white 2xl mt-1 mx-3 hover:text-gray-500 ${
+                pathname === "/books" ? "active" : ""
+              }`}
+            >
+              Find Books
+            </Link>
+          )}
+          {pathname === "/" ? (
+            <Link
+              href="/login"
+              className="text-white text-lg hover:text-gray-500"
+            >
+              Login
+            </Link>
+          ) : (
+            pathname !== "/students" && (
+              <button
+                onClick={handleLogout}
+                className="text-white text-lg hover:text-gray-500"
+              >
+                Logout
+              </button>
+            )
+          )}
         </div>
         <div className="md:hidden">
           <button
@@ -54,15 +70,30 @@ const Navbar = () => {
         {isOpen && (
           <div className="md:hidden absolute top-12 right-0 bg-gray-800 w-full h-full">
             <div className="flex flex-col items-center justify-center h-1/2 m-4">
-              <Link href="/books" className="text-white text-3xl my-4">
-                Find Books
-              </Link>
-              <button
-                onClick={handleLogout}
-                className="text-white text-3xl my-4 "
-              >
-                Logout
-              </button>
+              {pathname !== "/" && pathname !== "/students" && (
+                <Link
+                  href="/books"
+                  className={`text-white text-3xl my-4 ${
+                    pathname === "/books" ? "active" : ""
+                  }`}
+                >
+                  Find Books
+                </Link>
+              )}
+              {pathname === "/" ? (
+                <Link href="/login" className="text-white text-3xl my-4">
+                  Login
+                </Link>
+              ) : (
+                pathname !== "/students" && (
+                  <button
+                    onClick={handleLogout}
+                    className="text-white text-3xl my-4"
+                  >
+                    Logout
+                  </button>
+                )
+              )}
             </div>
           </div>
         )}
